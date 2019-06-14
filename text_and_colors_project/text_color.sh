f8c02fa2d83f44aacfc9e405f8114d704b34dbc6
domHenin <domhenin.517@gmail.com>
2019-06-14 16:24:40 -0400

    completed

---
+++ text_and_colors_project/text_color.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+NAME="dominic"  #RED
+HOBBY="programming"  #YELLOW
+DAY="thursday"  #BLUE
+
+echo
+
+echo "NAME:"
+echo -e "\e[31m$NAME\e[0m"
+
+echo
+
+echo "HOBBY:"
+echo -e "\e[33m$HOBBY\e[0m"
+
+echo
+
+echo "DAY of WEEK:"
+echo -e "\e[34m$DAY\e[0m"
+
+
+#TODO:
+# In this lab, you will simply create a BASH script that when run from the console contains the commands to print your
+# name in red
+# a hobby of your choice in yellow and
+# a day of the week in blue.
+# Submit the proper proof of concept paper
+# via the Canvas assignment drop box I have created. 100 points due June 18
+
+# STR="Hello World!"
+# echo $STR
