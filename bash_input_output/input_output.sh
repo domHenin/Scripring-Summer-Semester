6f891cfa97c1106b376f582621369a454e4453c3
domHenin <domhenin.517@gmail.com>
2019-07-01 21:39:26 -0400

    assignment complete

---
+++ bash_input_output/input_output.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+#echo "Inputs and Output"
+
+
+echo "Enter Name: "
+read NAME
+
+echo "Enter Number: "
+read NUMBER
+
+echo "Entered Name: ${NAME}"
+echo "Entered Number: ${NUMBER}"
+
+#ENDofSCRIPT
+
+
+
+
+
+
+# TODO:
+# In this assignment, you will work with inputs, variables and outputs.
+# You are to do the following and submit a proof of concept paper showing proficiency in the tasks at hand. 100 points
+#
+#1) Assign a variable to accept your name as an input.
+#
+#2) Assign a variable to take a number as an input.
+#
+#3) Print the contents of both variables to your screen.
