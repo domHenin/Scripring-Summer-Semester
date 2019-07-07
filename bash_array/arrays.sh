0022809299e52df929a10f19d858cb15d7bf0895
domHenin <domhenin.517@gmail.com>
2019-07-07 11:02:40 -0400

    completed Array. adding While Loop

--- bash_array/arrays.sh
+++ bash_array/arrays.sh
@@ -11,9 +11,21 @@ names[2]=vegeta
 names[3]=broly
 names[4]=gohan
 
+
+# calling certain array:
 echo ${names[3]}
 echo ${names[1]}
+echo
 
+# for loop example:
 #for i in {1..4}; do echo $i; done
 
+# for loop to iterate through set array:
 for i in {1..4}; do echo "User $i is ${names[i]}"; done
+
+#ENDofSCRIPT
+
+
+#TODO:
+# Please watch the following video and submit a proof of concept paper doing exactly what you see in the video.
+# Make sure to comment your code at the end of the paper to show proficiency in the task at hand
